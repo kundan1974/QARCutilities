382de4fbe797f4f0c77a90715c1e189226275ab1
Avani Dwivedi <dwivedi.aashi@gmail.com>
2022-06-23 17:50:05 +0100

    all the patients preprocessing completed

--- README.md
+++ README.md
@@ -10,17 +10,11 @@ All work done so far is available in this repo https://github.com/kundan1974/QAR
 Check out this for preprocessing overview
 https://github.com/kundan1974/QARC-E001/blob/master/README.md#image-preprocessing--labels
 
-## Misc Notes - Troubleshooting points
- - Need a requirements.txt to manage python dependencies. New module install needed
-   - numpy
-   - SimpleITK
-   - scipy
-   - scikit-image -> I had issue with scimage as I tried scimage instead of scikit-image as it was not obvious which one I needed
-   - pydicom
-   - matplotlib
- - There was circular dependency in ~/dev/github/QARCutilities/dcmrtstruct2nii/__init__.py 
-    for now removed circular inclusion of dcmrtstruct2nii
- - I have added a superset of requirements.txt using pip command once I had a working setup
-`pip freeze > requirements.txt`
- - Key python modules can now be installed by using script install_python_deps.sh
- - 
+## Python module dependencies
+All python module dependency has been cpatured in requirements.txt. You can use `install_deps.sh` shell script. Alternatively run following command to get all Python modules installed in one go
+`pip install -r requirements.txt`
+
+## Preprocessing all patient images
+Python script `preprocess.py` contains logic to preprocess all patient images. For every patient, once the numpy file has been successfully created, the original patient folder is moved to a specified completed folder. If any error is encountered during preprocessing than that patient folder is moved to an error folder under the specified completed folder
+
+
--- preprocess.py
+++ preprocess.py
@@ -102,49 +102,76 @@ import numpy as np
 import shutil
 
 # NOTE: Its assumed that RT Struct file will follow a naming convention of RS.<patient_id>.dcm
-rt_struct_file_format = "RS.{:s}.dcm"
+RT_STRUCT_FILE_FORMAT = "RS.{:s}.dcm"
 
 
 def valid_patient_id(patient_id):
-# TODO: check if it's a valid integer
 # Question: Can we assume that the patient ID will always be a 6 digit integer value?
-    return True
+    valid = True
+    if len(patient_id) != 6:
+        valid = False
+    else:
+        try:
+            patient_id = int(patient_id)
+        except ValueError:
+            # handle the exception
+            valid = False
+    return valid
+#method has been checked with incorrect type and incorrect length in jupyter notebook
+#Question: is try except the same as try catch but python syntax rather than java syntax
 
 
-def move_to_error_folder(patient_id_path, completed_dicom_images_path):
-# TODO: move input path into the error folder using shutil
-    pass
+def move_to_error_folder(patient_id_path, completed_dicom_images_path, patient_id):
+# first get error folder path
+    error_path = os.path.join(completed_dicom_images_path, "error")
+    error_patient_id_path = os.path.join(error_path, patient_id)
+    if os.path.exists(error_patient_id_path):
+        shutil.rmtree(error_patient_id_path, ignore_errors=True)
+    shutil.move(patient_id_path,error_path)
 
 
-def move_to_completed_folder(patient_id_path, completed_dicom_images_path):
-    # TODO: move the patient_id_path to the completed folder
-    pass
+def move_to_completed_folder(patient_id_path, completed_dicom_images_path, patient_id):
+    completed_patient_id_path = os.path.join(completed_dicom_images_path, patient_id)
+    if os.path.exists(completed_patient_id_path):
+        shutil.rmtree(completed_patient_id_path, ignore_errors=True)
+    shutil.move(patient_id_path, completed_dicom_images_path)
 
 
 def preprocess_all_dicom_images(dicom_images_path, completed_dicom_images_path, output_nympy_path):
     patient_ids = get_patient_ids(dicom_images_path)
 
     for patient_id in patient_ids:
-        patient_id_path = os.path.join(dicom_images_path, patient_id)
-        if valid_patient_id(patient_id):
-            # NOTE: Its assumed that RT Struct file will follow a naming convention of RS.<patient_id>.dcm
-            patient_rt_struct_file_name = rt_struct_file_format.format(patient_id)
-            patient_rt_struct_file_path = os.path.join(patient_id_path,patient_rt_struct_file_name)
-            # check if RS file exists in that patient
-            if not os.path.exists(patient_rt_struct_file_path):
-                print(f"Ignoring Patient Id {patient_id} as it does not have an RS file {patient_rt_struct_file_path}");
-                # move this folder to error folder
-                move_to_error_folder(patient_id_path, completed_dicom_images_path)
+        try:
+            preprocess_one_patient(completed_dicom_images_path, dicom_images_path, output_nympy_path, patient_id)
+        except Exception as e:
+            print(f"An error for patient_id {patient_id}.\n{e}")
+            # move this folder to error folder
+            patient_id_path = os.path.join(dicom_images_path, patient_id)
+            move_to_error_folder(patient_id_path, completed_dicom_images_path, patient_id)
+
+def preprocess_one_patient(completed_dicom_images_path, dicom_images_path, output_nympy_path, patient_id):
+    patient_id_path = os.path.join(dicom_images_path, patient_id)
+    if valid_patient_id(patient_id):
+        # NOTE: Its assumed that RT Struct file will follow a naming convention of RS.<patient_id>.dcm
+        patient_rt_struct_file_name = RT_STRUCT_FILE_FORMAT.format(patient_id)
+        patient_rt_struct_file_path = os.path.join(patient_id_path, patient_rt_struct_file_name)
+        # check if RS file exists in that patient
+        if os.path.exists(patient_rt_struct_file_path):
             patient_numpy_file_path = os.path.join(output_nympy_path, patient_id + ".npy")
             print(f'Going to generate cropped {patient_numpy_file_path}')
             img = preprocessDICOM.preprocess(patient_id_path, patient_rt_struct_file_path, zero=False)
+            # img = [0, 1, 2]
             np.save(patient_numpy_file_path, img)
             print(f'{patient_numpy_file_path} Cropped Successfully')
-            move_to_completed_folder(patient_id_path, completed_dicom_images_path)
+            move_to_completed_folder(patient_id_path, completed_dicom_images_path, patient_id)
         else:
-            print(f"Ignoring Patient Id {patient_id} as its not a valid 6 digit integer");
+            print(f"Ignoring Patient Id {patient_id} as it does not have an RS file {patient_rt_struct_file_path}");
             # move this folder to error folder
-            move_to_error_folder(patient_id_path, completed_dicom_images_path)
+            move_to_error_folder(patient_id_path, completed_dicom_images_path, patient_id)
+    else:
+        print(f"Ignoring Patient Id {patient_id} as its not a valid 6 digit integer");
+        # move this folder to error folder
+        move_to_error_folder(patient_id_path, completed_dicom_images_path, patient_id)
 
 
 def get_patient_ids(dicom_images_path):
